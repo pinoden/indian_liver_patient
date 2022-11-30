77d8debd94a45ab465da65797c00bb2a841a5464
pinoden <48227075+pinoden@users.noreply.github.com>
2022-11-30 15:29:53 -0500

    Delete playground.py

--- playground.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import scipy.sparse as sparse
-from sklearn import preprocessing
-
-lb = preprocessing.LabelBinarizer()
-print(lb.fit_transform(['yes', 'no', 'no', 'yes']))
